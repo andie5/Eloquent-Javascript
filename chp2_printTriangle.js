ea292269bf06d2344123f605e92db905b30592c1
Andie <andieasa@googlemail.com>
2015-04-04 22:16:27 +0100

    Chapter 2 exercises

---
+++ chp2_printTriangle.js
@@ -0,0 +1,14 @@
+// Chapter 2
+
+var trianglePart = "#";
+for(var x = 0; x<7; x++){
+  console.log(trianglePart);
+  trianglePart += "#";
+}
+
+var lastLine = "#######";
+var firstLine = "#";
+for (var number = 0; number < lastLine.length; number++){
+  console.log(firstLine);
+  firstLine += "#";
+}
